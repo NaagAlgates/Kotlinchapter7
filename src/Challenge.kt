ea4767ef3a26da14f8eb7d19734b198ef7ec0e74
NaagAlgates <nagharaj.cse@gmail.com>
2019-07-01 16:42:01 +1000

    Challenge: Handling a negative balance

--- src/Challenge.kt
+++ src/Challenge.kt
@@ -5,14 +5,15 @@ const val TAVERN_NAME = "Taernyl's Folly"
 var playerGold = 10
 var playerSilver = 10
 var dragonBreathQuantity = 5.0
-val onePint=0.125
+val onePint = 0.125
 val showPintCountThreshhold = 12
 fun main() {
     displayRemaningDragonsBreath(0)
     placeOrder("shandy,DRAGON'S BREATH; IT'S GOT WHAT ADVENTURES CRAVE,5.91")
-    displayRemaningDragonsBreath(13)
+    //displayRemaningDragonsBreath(13)
 }
-fun placeOrder(menuData:String){
+
+fun placeOrder(menuData: String) {
     try {
         val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
         val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
@@ -21,53 +22,60 @@ fun placeOrder(menuData:String){
         val type = data[0]
         val name = data[1]
         val price = data[2]*/
-        val (type,name,price) = menuData.split(',')
+        val (type, name, price) = menuData.split(',')
         val message = "Madrigal buys a ${name.split(';')[0].toLowerCase().capitalize()} ($type) for $price"
         val phrase = "Ah, delicious $name"
         println(message)
         performPurchase(price = price.toDouble())
         println(toDragonSpeak(phrase))
-    }catch (e:Exception){
+        performPurchase(price = price.toDouble())
+    } catch (e: Exception) {
         println(e)
         println("Oh No!. Details not fully entered.")
     }
 }
 
-fun toDragonSpeak(phrase:String)=phrase.replace(Regex("[aAeEiIoOuU]")){
-    when(it.value){
-        "a"->"4"
-        "e"->"3"
-        "i"->"1"
-        "o"->"0"
-        "u"->"|_|"
-        "A"->"4"
-        "E"->"3"
-        "I"->"1"
-        "O"->"0"
-        "U"->"|_|"
-        else->it.value
+fun toDragonSpeak(phrase: String) = phrase.replace(Regex("[aAeEiIoOuU]")) {
+    when (it.value) {
+        "a" -> "4"
+        "e" -> "3"
+        "i" -> "1"
+        "o" -> "0"
+        "u" -> "|_|"
+        "A" -> "4"
+        "E" -> "3"
+        "I" -> "1"
+        "O" -> "0"
+        "U" -> "|_|"
+        else -> it.value
     }
 }
-fun performPurchase(price: Double){
+
+fun performPurchase(price: Double) {
     displayBalance()
-    val totalPurse = playerGold+(playerSilver/100.0)
+    val totalPurse = playerGold + (playerSilver / 100.0)
     println("Total purse: $totalPurse")
     println("Purchasing item for $price")
-    val remainingBalance=totalPurse-price
-    println("Remaning Balance: ${"%.2f".format(remainingBalance)}")
-    val remaningGold = remainingBalance.toInt()
-    val remainingSilver = (remainingBalance%1*100).roundToInt()
-    playerGold=remaningGold
-    playerSilver=remainingSilver
-    displayBalance()
+    val remainingBalance = totalPurse - price
+    if (remainingBalance > 0) {
+        println("Remaning Balance: ${"%.2f".format(remainingBalance)}")
+        val remaningGold = remainingBalance.toInt()
+        val remainingSilver = (remainingBalance % 1 * 100).roundToInt()
+        playerGold = remaningGold
+        playerSilver = remainingSilver
+        displayBalance()
+    } else {
+        println("You don't have sufficient balance.")
+    }
 }
-private fun displayBalance(){
+private fun displayBalance() {
     println("Player's purse balance: Gold: $playerGold, Silver $playerSilver")
 }
-private fun displayRemaningDragonsBreath(purchasedquantity:Int){
+
+private fun displayRemaningDragonsBreath(purchasedquantity: Int) {
     dragonBreathQuantity -= (purchasedquantity * onePint)
     println("Remaining Breath (Gallons): $dragonBreathQuantity")
-    if(purchasedquantity>=showPintCountThreshhold){
-        println("Remaining Pints: ${(dragonBreathQuantity/onePint).roundToInt()}")
+    if (purchasedquantity >= showPintCountThreshhold) {
+        println("Remaining Pints: ${(dragonBreathQuantity / onePint).roundToInt()}")
     }
 }
